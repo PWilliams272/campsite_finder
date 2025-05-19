3c44cdf44fee9600b29e59c521c38ede6cdf055b
Peter Williams <peterwilliams@peters-air.lan>
2025-05-18 21:32:54 -0700

    Moving web_app into campsite_finder repo

--- run_app.py
+++ run_app.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask
-from app import campsite_bp
+from campsite_finder.app import campsite_bp
 
 app = Flask(__name__)
 app.register_blueprint(campsite_bp, url_prefix="/")
